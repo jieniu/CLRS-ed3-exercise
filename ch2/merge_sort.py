7b608fe156673840bf0de8c2625773f0bf5f9739
jieniu <yajie.feng@gmail.com>
2017-10-03 14:47:23 +0800

    complete merge sort and add inversions to it

--- ch2/merge_sort.py
+++ ch2/merge_sort.py
@@ -11,23 +11,49 @@ def merge(arr, p, q, r):
 
     i = 0
     j = 0
+    inversions = 0
     while i < len(left):
         if j >= len(right):
             break
-        if left[i] < right[j]:
-            arr[i+j] = left[i]
+        if left[i] <= right[j]:
+            arr[p+i+j] = left[i]
             i += 1
         else:
-            arr[i+j] = right[j]
+            arr[p+i+j] = right[j]
             j += 1
+            inversions += len(left) - i
 
     if i == len(left):
-        arr[i+j:] = right[j:]
+        arr[p+i+j:r] = right[j:len(right)]
     else:
-        arr[i+j:] = left[i:]
+        arr[p+i+j:r] = left[i:len(left)]
+
+    return inversions
+
+
+def merge_sort(arr, p, r):
+    inversions = 0
+    if p+1 >= r:
+        return 0
+
+    q = (p + r)/2
+    inversions += merge_sort(arr, p, q)
+    inversions += merge_sort(arr, q, r)
+    inversions += merge(arr, p, q, r)
+    return inversions
 
 
 if __name__ == "__main__":
     a = [0,0,1,2,2,3,1,4,5,7,8,9,99,100,101]
-    merge(a, 0, 6, 15)
+    merge(a, 0, 6, len(a))
     print a
+
+    a = [5,2,4,7,1,3,2,6]
+    inv = merge_sort(a, 0, len(a))
+    print a
+    print inv
+
+    a = [2,3,8,6,1]
+    inv = merge_sort(a, 0, len(a))
+    print inv
+

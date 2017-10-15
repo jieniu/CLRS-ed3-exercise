4a643877136f3e4880688f2096e1802c110a92fd
jieniu <yajie.feng@gmail.com>
2017-10-15 23:27:06 +0800

    add linear method to implement find_max_array

--- ch4/find_max_sub_array.py
+++ ch4/find_max_sub_array.py
@@ -8,19 +8,14 @@ def findMaximumSubArray(arr, start, end):
     if start == end - 1:
         return (start, end, arr[start])
 
-    # change the base
-    #if (end - start) <= 31:
-    #    (s, e, sum) = findMaximumSubArrayBruteForceV(arr[start:end])
-    #    return (s+start, e+start, sum)
-
     mid = (start + end) / 2
     (left_begin, left_end, left_sum) = findMaximumSubArray(arr, start, mid)
     (right_begin, right_end, right_sum) = findMaximumSubArray(arr, mid, end)
     (cross_left, cross_right, cross_sum) = findMaxCrossingSubarray(arr[start:end], mid - start)
 
-    if left_sum > right_sum and left_sum > cross_sum:
+    if left_sum >= right_sum and left_sum >= cross_sum:
         return (left_begin, left_end, left_sum)
-    elif right_sum > left_sum and right_sum > cross_sum:
+    elif right_sum >= left_sum and right_sum >= cross_sum:
         return (right_begin, right_end, right_sum)
     else:
         return (start+cross_left, start+cross_right, cross_sum)
@@ -33,7 +28,7 @@ def findMaxCrossingSubarray(array, mid):
     while offset > 0:
         offset -= 1
         left_sum += array[offset]
-        if left_sum > left_max:
+        if left_sum >= left_max:
             left_max = left_sum
             begin = offset
 
@@ -44,7 +39,7 @@ def findMaxCrossingSubarray(array, mid):
     while offset < len(array) - 1:
         offset += 1
         right_sum += array[offset]
-        if right_sum > right_max:
+        if right_sum >= right_max:
             right_max = right_sum
             end = offset
 
@@ -60,20 +55,46 @@ def findMaximumSubArrayBruteForceV(arr):
         sub_sum = 0
         for j in range(i, len(arr)):
             sub_sum += arr[j]
-            if sub_sum > max_sub:
+            if sub_sum >= max_sub:
                 max_sub = sub_sum
                 begin = i
                 end = j
     return (begin, end, max_sub)
 
+# 线性复杂度求最大子数组问题
+def findMaximumSubArrayLinear(arr):
+    left = 0
+    right = 0
+    temp_left = 0
+    temp_sum = 0
+    sum_max = arr[0]
+
+    for i in range(0, len(arr)):
+        if temp_sum + arr[i] <= arr[i]:
+            temp_left = i
+            temp_sum = arr[i]
+        else:
+            temp_sum += arr[i]
+
+        if temp_sum > sum_max:
+            sum_max = temp_sum
+            right = i
+            left = temp_left
+
+    return (left, right, sum_max)
 
 def computeCrossN():
-    for i in range(1, 100000):
+    for i in range(2, 50):
         random.seed(i)
         arr = []
         for j in range(0, i):
             num = random.randint(-1000, 1000)
             arr.append(num)
+
+        start = datetime.datetime.now()
+        findMaximumSubArrayLinear(arr)
+        end = datetime.datetime.now()
+        delta3 = (end - start).microseconds * 1.0 / 1000
 
         start = datetime.datetime.now()
         findMaximumSubArray(arr, 0, len(arr))
@@ -87,25 +108,37 @@ def computeCrossN():
 
         if delta1 < delta2:
             print "crossover n = %d" % i
-            print "O(nlogN) %f, O(N^2) %f" % (delta1, delta2)
+            print "O(nlogN) %f, O(N^2) %f, O(N) %f" % (delta1, delta2, delta3)
             break
         else:
-            print "O(nlogN) %f, O(N^2) %f" % (delta1, delta2)
-
+            print "O(nlogN) %f, O(N^2) %f, O(N) %f" % (delta1, delta2, delta3)
 
 
 if __name__ == "__main__":
+
+    computeCrossN()
+
     arr = [1,2,3,4,-7,6]
     print findMaxCrossingSubarray(arr, 3)
+    print findMaximumSubArrayBruteForceV(arr)
+    print findMaximumSubArrayLinear(arr)
+    arr = [-1,-2,-3,-4,-7,-6]
+    print findMaximumSubArrayLinear(arr)
+    arr = [20,-3,-1,-7,11,1,-22]
+    print findMaximumSubArrayLinear(arr)
+    arr = [20,-3,-1,-7,11,1,-21, 1, 2,19]
+    print findMaximumSubArrayLinear(arr)
+    print findMaximumSubArray(arr, 0, len(arr))
+    print findMaximumSubArrayBruteForceV(arr)
 
     # example in page 70
     arr = [13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7]
     print findMaximumSubArray(arr, 0, len(arr))
     print findMaximumSubArrayBruteForceV(arr)
+    print findMaximumSubArrayLinear(arr)
 
     arr = [-1,-2,-3,-4,-5]
     print findMaximumSubArrayBruteForceV(arr)
 
     arr = [-2,-3,-4,-5, -1]
     print findMaximumSubArrayBruteForceV(arr)
-    computeCrossN()
